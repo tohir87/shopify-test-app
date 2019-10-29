7a957913a929a99203960687d307bb02e0204780
Tohir Omoloye <otcleantech@gmail.com>
2019-10-29 15:01:39 +0000

    removed cached files, added another annotated layout

--- pages/annotated-layout.js
+++ pages/annotated-layout.js
@@ -1,9 +1,27 @@
-import { Card, Layout, Page} from '@shopify/polaris';
+import {
+	Button,
+	Card,
+	Form,
+	FormLayout,
+	Layout,
+	Page,
+	SettingToggle,
+	Stack,
+	TextField,
+	TextStyle,
+  } from '@shopify/polaris';
 
 class AnnotetedLayout extends React.Component {
-	state = {};
+	state = {
+		discount: '10%',
+		enabled: false,
+	};
 
 	render() {
+		const { discount, enabled } = this.state;
+		const contentStatus = enabled ? 'Disable' : 'Enable';
+		const textStatus = enabled ? 'enabled' : 'disabled';
+
 		return (
 			<Page>
 				<Layout>
@@ -12,14 +30,61 @@ class AnnotetedLayout extends React.Component {
 						description="Add a product to sample App, it will automatically be discounted"
 					>
 						<Card sectioned>
-							<div>Card</div>
+							<Form onSubmit={this.handleSubmit}>
+								<FormLayout>
+									<TextField
+										value={discount}
+										onChange={this.handleChange('discount')}
+										label="Discount percentage"
+										type="discount"
+									/>
+									<Stack distribution="trailing">
+										<Button primary submit>
+										Save
+										</Button>
+									</Stack>
+								</FormLayout>
+							</Form>
 						</Card>
 
+					</Layout.AnnotatedSection>
+
+					<Layout.AnnotatedSection title="Price updates"
+						description="Temporarily disable price updates"
+					>
+						<SettingToggle
+							action={{
+								content: contentStatus,
+								onAction: this.handleToggle,
+							}}
+							enabled={enabled}
+						>
+							This setting is{' '}
+							<TextStyle variation="strong">{textStatus}</TextStyle>.
+						</SettingToggle>
 					</Layout.AnnotatedSection>
 				</Layout>
 			</Page>
 		);
 	}
+
+	handleSubmit = () => {
+	this.setState({
+		discount: this.state.discount,
+		});
+		console.log('submission', this.state);
+	};
+
+	handleChange = (field) => {
+		return (value) => this.setState({ [field]: value });
+	};
+
+	handleToggle = () => {
+		this.setState( ({enabled}) =>{
+			return {enabled: !enabled};
+		});
+	};
 }
+
 
 export default AnnotetedLayout;
